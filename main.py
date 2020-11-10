36a3aa373c4bce26a97680a8011f091e78b98851
tomstewrt <thomasastewart99@gmail.com>
2020-11-10 21:09:01 +0000

    Added gallery, updated upload. Added nav bar, header, footer and bootstrap

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 datastore_client = datastore.Client()
 firebase_request_adapter = requests.Request()
 
-@app.route('/')
+
+@app.route("/")
 def root():
     # Verify Firebase auth.
-    id_token = request.cookies.get('token')
+    id_token = request.cookies.get("token")
     error_message = None
     claims = None
-    times = None
+    time = None
 
     if id_token:
         try:
@@ -25,35 +26,112 @@ def root():
             # session store (see for instance
             # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
             claims = google.oauth2.id_token.verify_firebase_token(
-                id_token, firebase_request_adapter)
-
-            store_time(claims['email'], datetime.datetime.now())
-            times = fetch_times(claims['email'], 10)
+                id_token, firebase_request_adapter
+            )
+            time = fetch_time(claims["email"])
         except ValueError as exc:
-            # This will be raised if the token is expired or any other 
+            # This will be raised if the token is expired or any other
             # verification checks fail
-            error_message = str(exc)     
-    
-    return render_template('index.html', user_data=claims, 
-    times=times, error_message=error_message)
+            error_message = str(exc)
+
+    return render_template(
+        "index.html", user_data=claims, time=time, error_message=error_message
+    )
+
+
+def update_time(entity, time):
+    entity.update({"timestamp": time})
+    datastore_client.put(entity)
+    return
+
 
 def store_time(email, dt):
-    entity = datastore.Entity(key=datastore_client.key('User', email, 'visit'))
-    entity.update({
-        'timestamp': dt
-    })
+    entity = datastore.Entity(key=datastore_client.key("User", email, "visit"))
+    entity.update({"timestamp": dt})
 
     datastore_client.put(entity)
 
-def fetch_times(email, limit):
-    ancestor = datastore_client.key('User', email)
-    query = datastore_client.query(kind='visit', ancestor=ancestor) 
-    query.order = ['-timestamp']
 
-    times = query.fetch(limit=limit)
+def fetch_time(email):
+    ancestor = datastore_client.key("User", email)
+    query = datastore_client.query(kind="visit", ancestor=ancestor)
+    result = list(query.fetch(limit=1))
+    time = datetime.datetime.now()
+    # If first visit store time
+    if len(result) == 0:
+        store_time(email, time)
+    else:
+        update_time(result[0], time)
+        time = result[0]
 
-    return times
+    return time
 
-if __name__ == '__main__':
+
+# [START upload file code]
+@app.route("/uploadfile")
+def upload_file():
+    # Verify Firebase auth.
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions)
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter
+            )
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+    else:
+        return redirect("/")
+
+    return render_template(
+        "uploadfile.html", user_data=claims, error_message=error_message
+    )
+
+
+@app.route("/gallery")
+def gallery():
+    # Get paged gallery data and display to users
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+    results = None
+
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions)
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter
+            )
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+    else:
+        return redirect("/")
+
+    query = datastore_client.query(kind="gallery")
+    results = query.fetch()
+
+    render_template(
+        "gallery.html", user_data=claims, error_message=error_message, results=results
+    )
+
+
+# [END upload file code]
+
+if __name__ == "__main__":
     # Used when running locally only, when deploying to GAE a webserver serves the app
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ static/upload.js
@@ -0,0 +1,30 @@
+// connect to appropriate page elements via the DOM
+var fileButton = document.getElementById('fileButton');
+// add an event listener to the 
+fileButton.addEventListener('change', function (e) {
+    var file = e.target.files[0];
+    // Create file metadata including the content type
+    var metadata = {
+        customMetadata: {
+            userid: '1',
+            activity: 'Course Work',
+            notes: 'A fixed message about this file'
+        }
+    };
+    // set  upload locaation
+    var storageRef = firebase.storage().ref('img/' + file.name);
+    //upload file and metadata
+    var task = storageRef.put(file, metadata);
+    // set callback event + function, error function and complete function
+    task.on('state_changed',
+        function error(err) {
+            alert("File upload failed:" + err.message);
+
+        }, function complete() {
+            alert("File upload successful");
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            task.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+                console.log('File available at', downloadURL);
+            });
+        });
+});
